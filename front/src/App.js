646ad248585d13c0a7b23b5cd5de97dd8ed1c592
bbpandc <haeminsia@gmail.com>
2024-09-08 21:15:40 +0900

    추가페이지css

--- front/src/App.js
+++ front/src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import QuestionPage from './pages/QuestionPage/QuestionPage';
 import BackgroundPage from './pages/BackgroundPage/BackgroundPage';
+import RegisterMemoPage from './pages/RegisterMemoPage/RegisterMemoPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/question' element={<QuestionPage />} />
+          <Route path='/memo/add' element={<RegisterMemoPage />} />
         </Routes>
       </BackgroundPage>
     </>
