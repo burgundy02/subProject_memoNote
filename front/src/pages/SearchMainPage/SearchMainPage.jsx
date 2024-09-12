2ec4fe446e23ebfe614d7f7293490e2c3e8ea245
Skeat <iu_love@kakao.com>
2024-09-12 21:52:58 +0900

    디자인 변경

--- front/src/pages/SearchMainPage/SearchMainPage.jsx
+++ front/src/pages/SearchMainPage/SearchMainPage.jsx
@@ -1,10 +1,19 @@
 /** @jsxImportSource @emotion/react */
+import { useNavigate } from 'react-router-dom';
 import Search from '../../components/Search/Search';
 import * as s from './style';
 
 function SearchMainPage(props) {
+    const navigate = useNavigate();
+
+    const handleButtonOnClick = () => {
+        navigate("/");
+    }
     return (
         <div css={s.layout}>
+            <div css={s.buttonBox}>
+                <button onClick={handleButtonOnClick}>취소</button>
+            </div>
             <Search>
 
             </Search>
