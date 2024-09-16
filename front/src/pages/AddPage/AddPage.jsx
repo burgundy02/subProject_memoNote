928a29add2dcfef3125ff6e851d457e669764a40
gyeolhwi <gyeori0626@gmail.com>
2024-09-16 23:16:19 +0900

    gyeolhwi 풀 받기 전

--- front/src/pages/AddPage/AddPage.jsx
+++ front/src/pages/AddPage/AddPage.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 /** @jsxImportSource @emotion/react */
 import * as s from "./style";
 import { useNavigate } from 'react-router-dom';
+import { addQuestionApi } from '../../apis/memoApi/memoApi';
 
 function AddPage(props) {
-    const navigate = useNavigate();
 
+    const [questionInput, setQuestionInput] = useState({
+        writer: "default",
+        quest: "",
+        answer: "",
+        explainMemo: "",
+    });
+
+    const navigate = useNavigate();
     const handleBackButtonOnClick = () => {
         navigate("/");
     }
 
+    const handleInputOnChnage = (e) => {
+        setQuestionInput(inputs => ({
+            ...inputs,
+            [e.target.name]: e.target.value
+        }))
+    }
+    const handleSubmitClick = async () => {
+        const addData = await addQuestionApi(questionInput);
+        console.log(addData);
+    }
     return (
         <div css={s.layout}>
             <div css={s.backButtonBox}>
@@ -22,24 +40,24 @@ function AddPage(props) {
                 <div css={s.inputBox1}>
                     <span>문제</span>
                     <div>
-                        <input type="text" />
+                        <input name='quest' type="text" onChange={handleInputOnChnage} value={questionInput.quest} />
                     </div>
                 </div>
                 <div css={s.inputBox1}>
                     <span>정답</span>
                     <div>
-                        <input type="text" />
+                        <input name='answer' type="text" onChange={handleInputOnChnage} value={questionInput.answer} />
                     </div>
                 </div>
                 <div css={s.inputBox2}>
                     <span>예제</span>
                     <div>
-                        <textarea name=""></textarea>
+                        <textarea name='explainMemo' onChange={handleInputOnChnage} value={questionInput.explainMemo} />
                     </div>
                 </div>
             </div>
             <div>
-                <button css={s.addButton}>추가</button>
+                <button css={s.addButton} onClick={handleSubmitClick}>추가</button>
             </div>
         </div>
     );
